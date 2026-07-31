e38a10ae804e72b4be3f009398e2321ee30fba61
M S <msds3272021@gmail.com>
2026-07-31 11:22:11 +0800

    Import removeItem and updateQuantity from CartSlice

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import addItem from './CartSlice';
+Import removeItem from './CartSlice';
+import updateQuantity from './CartSlice;
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
