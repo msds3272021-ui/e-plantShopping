f2b7596ce3b5c887e3da781e135f8b1438dcd45c
M S <msds3272021@gmail.com>
2026-07-31 11:24:47 +0800

    Import additional functions from CartSlice

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+import addItem from './CartSlice';
+Import removeItem from './CartSlice';
+import updateQuantity from './CartSlice';
  const store = configureStore({
     reducer: {
         cart: cartReducer,
